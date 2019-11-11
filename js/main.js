78f6d4a93eaee0868bb29ab84fcde7954a1ffd2f
Tony Saad <tony@wego.com>
2019-11-11 23:56:17 +0100

    Soundspot

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var API_KEY = 'BQDTFydJmcREN-pGwqAokeFS0VROJM8RkokeXNnbX41KaqCse7aDpMOGqKYUNPFS_5zLjCcMaYImZY0twWlXz5vnwAHyxDMqyK7FhBzg7FH6Dno73sg9XtI3ZVtLQWaKxyhesCxRTu9QR_P2miYdymmoBT54SsE';
+/*
+مثال من هذا الموقع 
+Spotify: https://developer.spotify.com/console/get-search-item/?q=cheb%20khaled&type=artist&market=&limit=&offset=
+عند الضغط على زر:
+Try It
+سترى الرد من الخادم
+
+ولفهم كيفية إرسال الطلب يجب فتح هذا الرابط
+https://developer.spotify.com/documentation/web-api/reference/search/search/
+*/
+var API_ENDPOINT = 'https://api.spotify.com/v1/search';
